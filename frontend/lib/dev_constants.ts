67e90869a9cc650a3102a56605010d92a62db749
Sharath Palathingal <palathingal@gatech.edu>
2022-08-09 03:46:21 -0700

    Added keys

--- frontend/lib/dev_constants.ts
+++ frontend/lib/dev_constants.ts
@@ -7,12 +7,13 @@ enum listTypes {
 }
 
 const section0: Array<any> = [
-  { id: "firstName", label: "First Name", required: true },
-  { id: "lastName", label: "Last Name", required: true },
-  { id: "preferredName", label: "Preferred Name", required: false },
-  { id: "gtEmail", label: "GT Email", required: true },
-  { id: "personalEmail", label: "Personal Email", required: false },
+  { key: 0, id: "firstName", label: "First Name", required: true },
+  { key: 1, id: "lastName", label: "Last Name", required: true },
+  { key: 2, id: "preferredName", label: "Preferred Name", required: false },
+  { key: 3, id: "gtEmail", label: "GT Email", required: true },
+  { key: 4, id: "personalEmail", label: "Personal Email", required: false },
   {
+    key: 5,
     id: "preferredPronouns",
     label: "Preferred Pronouns",
     required: true,
@@ -21,6 +22,7 @@ const section0: Array<any> = [
     radioOptions: ["he/him/his", "she/her/hers", "they/them/theirs"],
   },
   {
+    key: 6,
     id: "gender",
     label: "Gender",
     required: true,
@@ -29,6 +31,7 @@ const section0: Array<any> = [
     radioOptions: ["Male", "Female", "Prefer not to say"],
   },
   {
+    key: 7,
     id: "ethnicity",
     label: "Please indicate the race/ethnicity you identify yourself with",
     required: true,
@@ -45,6 +48,7 @@ const section0: Array<any> = [
     ],
   },
   {
+    key: 8,
     id: "degree",
     label: "What is your degree program?",
     required: true,
@@ -52,29 +56,40 @@ const section0: Array<any> = [
     hasOther: false,
     radioOptions: ["Undergraduate", "Masters", "PhD"],
   },
-  { id: "year", label: "What year are you?", required: true, numeric: true },
   {
+    key: 9,
+    id: "year",
+    label: "What year are you?",
+    required: true,
+    numeric: true,
+  },
+  {
+    key: 10,
     id: "major",
     label: "What major are you, or what is your research focus?",
     required: true,
   },
   {
+    key: 11,
     id: "vip",
     label: "Are you part of the VIP for Bits of Good?",
     required: false,
   },
   {
+    key: 12,
     id: "otherCommitments",
     label: "What else are you involved in on- or off-campus?",
     required: true,
   },
   {
+    key: 13,
     id: "hpw",
     label: "How many hours/week can you commit to Bits of Good?",
     required: true,
     numeric: true,
   },
   {
+    key: 14,
     id: "meetingAvailability",
     label: "Can you attend meetings on Tuesdays from 6:30 - 8:00 PM?",
     required: true,
@@ -83,11 +98,13 @@ const section0: Array<any> = [
     radioOptions: ["Yes", "No"],
   },
   {
+    key: 15,
     id: "otherAvailabilityConcerns",
     label: "Note any other availability concerns here :)",
     required: false,
   },
   {
+    key: 16,
     id: "previouslyApplied",
     label:
       "Were you previously a member of Bits of Good prior to " +
@@ -99,6 +116,7 @@ const section0: Array<any> = [
 
 const section1: Array<any> = [
   {
+    key: 17,
     id: "gq1",
     label:
       "What motivates you to join Bits of Good? " +
@@ -107,6 +125,7 @@ const section1: Array<any> = [
     required: true,
   },
   {
+    key: 18,
     id: "gq2",
     label:
       "While you’re in Bits of Good you’ll be working on a team. " +
@@ -115,6 +134,7 @@ const section1: Array<any> = [
     required: true,
   },
   {
+    key: 19,
     id: "gq3",
     label:
       "If you had all the time and all the resources in the world, what would you do? [100 words]",
@@ -124,11 +144,13 @@ const section1: Array<any> = [
 
 const section2: Array<any> = [
   {
+    key: 20,
     id: "psq1",
     label: "Are you familiar with web-dev? [25 words]",
     required: true,
   },
   {
+    key: 21,
     id: "psq2",
     label:
       "Would you describe yourself as a Frontend, Backend or Fullstack engineer?",
@@ -138,6 +160,7 @@ const section2: Array<any> = [
     radioOptions: ["Frontend", "Backend", "Fullstack"],
   },
   {
+    key: 22,
     id: "psq3",
     label:
       "What is the most technically challenging project that you have worked on? " +
@@ -145,12 +168,14 @@ const section2: Array<any> = [
     required: true,
   },
   {
+    key: 23,
     id: "psq4",
     label:
       "If you don’t have a project to talk about, or fear that your particular project in the previous question doesn’t demonstrate your web development capabilities to the best of your ability, please take this simple take-home assessment: https://github.com/GTBitsOfGood/fall2021-dev-takehome. After you have finished, please discuss your home assessment below (max 1200 characters) and include a link to your public repository.",
     required: true,
   },
   {
+    key: 24,
     id: "psq5",
     label:
       "You’ve been assigned a ticket as a part of your first sprint at Bits of Good " +
@@ -161,32 +186,38 @@ const section2: Array<any> = [
 
 const section3: Array<any> = [
   {
+    key: 25,
     id: "oq1",
     label:
       "Link to your resume (we recommend uploading it to google driving and sharing)",
     required: true,
   },
   {
+    key: 26,
     id: "oq2",
     label: "Link to your GitHub",
     required: false,
   },
   {
+    key: 27,
     id: "oq3",
     label: "Link to your LinkedIn",
     required: false,
   },
   {
+    key: 28,
     id: "oq4",
     label: "Link to your Personal site/Portfolio",
     required: false,
   },
   {
+    key: 29,
     id: "oq5",
     label: "Any other links?",
     required: false,
   },
   {
+    key: 30,
     id: "oq6",
     label:
       "Anything you'd like to add or note that wasn't conveyed in the application?",
