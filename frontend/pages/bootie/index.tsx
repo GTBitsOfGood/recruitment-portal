472027a90fd68cfc4d0a514d37145b0d7f1ebf88
Sharath Palathingal <palathingal@gatech.edu>
2023-01-01 10:30:14 +0400

    Fixed dates on bootie apps

--- frontend/pages/bootie/index.tsx
+++ frontend/pages/bootie/index.tsx
@@ -57,7 +57,7 @@ const Dev: NextPage = () => {
             This is the application to be a Bits of Good <b>
               Dev Bootcamper
             </b>{" "}
-            for Fall 2022!
+            for Spring 2023!
             <br />
             <a
               target="_blank"
@@ -68,9 +68,9 @@ const Dev: NextPage = () => {
             </a>{" "}
             to learn more about <i>booties</i> at Bits of Good &#128187;
             <h3>Important dates</h3>
-            General Deadline: 11:59 PM, August 30, 2022
+            General Deadline: 11:59 PM, January 17, 2023
             <br />
-            Latest Decision Date: September 5, 2022
+            Latest Decision Date: January 20, 2023
             <br />
             <br />
             Note: If you are accepted, we do have a mandatory Launch Party at
